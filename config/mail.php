66bd61ae53500e43f4d076382f74e7156e3a2e5c
Dylan Pierce <me@dylanjpierce.com>
2024-12-10 08:49:09 -0500

    postmark

--- config/mail.php
+++ config/mail.php
@@ -109,8 +109,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'hello@getverdict.com'),
+        'name' => env('MAIL_FROM_NAME', 'Real ID'),
     ],
 
 ];
