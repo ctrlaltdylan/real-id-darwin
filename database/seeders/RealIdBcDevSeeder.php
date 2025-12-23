fa3893503b96ebe4f72ad4741884e849bee1c82c
Dylan Pierce <me@dylanjpierce.com>
2025-12-23 12:50:21 -0500

    Adding seeder for dev store

---
+++ database/seeders/RealIdBcDevSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Shop;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class RealIdBcDevSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $shop = Shop::firstOrCreate(
+          ['name' => 'nug4ejpavx'],
+          [
+            'title' => 'Real ID BC Dev',
+          ]
+        );
+
+        User::firstWhere('email', 'dylan@getverdict.com')->shops()->attach($shop);
+    }
+}
